00c058f1de6c5e01b8ec8fbf3eef1badf24b0ec8
hnslmp <ancemo14@gmail.com>
2021-12-11 11:35:13 +0700

    Selesai design login + button disabled kalau field masih kosong

--- app/src/main/java/com/example/finalproject2_e_commerce_app/MainActivity.java
+++ app/src/main/java/com/example/finalproject2_e_commerce_app/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.finalproject2_e_commerce_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_username,et_password;
+    private Button bt_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_username=findViewById(R.id.editText_username);
+        et_password=findViewById(R.id.editText_password);
+        bt_login=findViewById(R.id.button_login);
+
+
+        et_username.addTextChangedListener(loginTextWatcher);
+        et_password.addTextChangedListener(loginTextWatcher);
     }
+
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String usernameInput = et_username.getText().toString().trim();
+            String passwordInput = et_password.getText().toString().trim();
+
+            bt_login.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 }
