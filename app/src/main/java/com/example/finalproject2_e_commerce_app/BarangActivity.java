8b6bc65694e898ab57514674b8897b7cabb23376
RickyBR <berlandoricky@gmail.com>
2021-12-14 10:09:28 +0700

    selesai desain lagi

---
+++ app/src/main/java/com/example/finalproject2_e_commerce_app/BarangActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalproject2_e_commerce_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BarangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barang);
+    }
+}
